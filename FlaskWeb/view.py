6eb0030dc9b7074bc7cd69b12a87349c043040f2
DoHuy <loldh321@gmail.com>
2021-11-04 18:56:18 +0800

    fix bug

--- FlaskWeb/view.py
+++ FlaskWeb/view.py
@@ -6,3 +6,23 @@ views = Blueprint('view', __name__)
 @views.route('/')
 def home_page():
     return render_template('home.html')
+
+@views.route('/work')
+def work_page():
+    return render_template('work.html')
+
+@views.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@views.route('/work/worksingle')
+def work_single_page():
+    return render_template('work-single.html')
+
+@views.route('/work/pricing')
+def pricing_page():
+    return render_template('pricing.html')
+
+@views.route('/work/contact')
+def contact_page():
+    return render_template('contact.html')
