059ba79a30288aae8333920520870d9ba2892e02
moclaw <mocduonglam86@gmail.com>
2021-11-07 22:28:36 +0700

    update boostrap icon

--- FlaskWeb/__init__.py
+++ FlaskWeb/__init__.py
@@ -1,12 +1,26 @@
-from flask import Flask, url_for
+from flask import Flask
+from flask_sqlalchemy import sqlalchemy
+from os import path
+
+db = sqlalchemy
+DB_NAME = "database.db"
 
 
 def creat_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'adaddwqdasdasd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .view import views
     from .auth import auth
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(auth, url_prefix='/')
+    from .models import User, Note
+
     return app
+def create_database(app):
+    if not path('website/'+DB_NAME):
+        db.create_all(app=app)
+        print('Create Database!')
+
--- FlaskWeb/auth.py
+++ FlaskWeb/auth.py
@@ -1,11 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
+import re
 
-auth = Blueprint('admin', __name__)
+auth = Blueprint('auth', __name__)
 
 
-@auth.route("/login")
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>login</p>"
+    return render_template('login.html', boolean=True)
 
 
 @auth.route("/logout")
@@ -13,6 +14,34 @@ def logout():
     return "<p>logout</p>"
 
 
-@auth.route("/signin")
+@auth.route("/register", methods=['GET', 'POST'])
 def signin():
-    return "<p>signin</p>"
+    regex = re.compile('[@_!#$%^&*()<>?/|}{~:]')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        last_name = request.form.get('lastName')
+        password = request.form.get('password1')
+        password_confirm = request.form.get('password2')
+        if len(email) < 3:
+            flash('This is not mail, try again!', category='error')
+        elif len(first_name) <= 2:
+            flash('No one  have a First Name have 2 character, Bro get some char pls!!', category='error')
+            if not first_name.isalpha():
+                flash('Serious? Name have number??', category='error')
+        elif len(last_name) <= 2:
+            flash('No one  have a Last Name have 2 character, Bro get some char pls!!', category='error')
+            if not last_name.isnumeric():
+                flash('Serious? Name have number??', category='error')
+        elif password != password_confirm:
+            flash('Password don\'t match', category='error')
+            if len(password) < 6 or len(password_confirm) < 6:
+                if regex.search(password) != None and regex.search(password_confirm) != None:
+                    flash('Your password too strong, so I can\'t let you through', category='error')
+                    if password.islower() or password_confirm.islower():
+                        flash('Your password too strong, so I can\'t let you through', category='error')
+                    return
+        else:
+            flash('Account created!', category='success')
+        print(request.form)
+    return render_template('register.html')
